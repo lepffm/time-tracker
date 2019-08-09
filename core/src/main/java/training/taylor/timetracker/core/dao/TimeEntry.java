ffa102650dd89305fa881917c880f9d02f0f1b94
Your Name <you@example.com>
2019-08-09 06:16:49 +0000

    a

--- core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
+++ core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
@@ -40,10 +40,6 @@ public class TimeEntry {
 
     @Override
     public String toString() {
-        return "TimeEntry{" +
-                "description='" + description + "\'\n" +
-                ", rate=" + rate + "\n" +
-                ", time=" + time + "\n" +
-                '}\n';
+        return "TimeEntry{" + "description='" + description + "\'" + ", rate=" + rate + ", time=" + time + '}';
     }
 }
